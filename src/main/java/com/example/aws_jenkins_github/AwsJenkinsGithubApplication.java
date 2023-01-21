b2a6dd86d303edddb026d3fe0faa826565cefda8
heesootory <93choiheesoo@gmail.com>
2023-01-21 12:13:06 +0900

    test

---
+++ src/main/java/com/example/aws_jenkins_github/AwsJenkinsGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_jenkins_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJenkinsGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsJenkinsGithubApplication.class, args);
+    }
+
+}
